029378db3550c622e7e7b3ed318dc81020bff04d
Johannes-Michael <johannesmichael1993@gmail.com>
2016-12-06 14:32:43 +0100

    Added getNextBatch() method without TensorFlow

--- util/ImageLoader.py
+++ util/ImageLoader.py
@@ -1,20 +1,27 @@
 from __future__ import print_function
-from PIL import Image
+# from PIL import Image
 import tensorflow as tf
+from scipy import misc
+from tensorflow.python.framework import dtypes
 import numpy as np
 import os
 
-num_epochs = 10
+num_epochs = 3
 filename = "./resources/list_all.txt"
 
-class ImageLoader:
 
+class ImageLoader:
     def __init__(self, path, batchSize):
         self._path = path
         self._batchSize = batchSize
-        self._noImages = 0
         self._index_in_epoch = 0
         self._epochs_completed = 0
+
+    def getBatchSize(self):
+        return self._batchSize
+
+    def getEpochsCompleted(self):
+        return self._epochs_completed
 
     def read_labeled_image_list(self, image_list_file):
 
@@ -37,97 +44,108 @@ class ImageLoader:
             tmp.close()
         f.close()
 
-        self._noImages = len(filenames)
         self._fileNames = filenames
         self._labels = labels
 
-    def read_images_from_disk(self, input_queue):
+    def getNextBatchTF(self):
 
-        """Consumes a single filename and label as a ' '-delimited string.
-        Args:
-          filename_and_label_tensor: A scalar string tensor.
-        Returns:
-          Two tensors: the decoded image, and the string label.
-        """
-        label = input_queue[1]
-        file_contents = tf.read_file(input_queue[0])
-        example = tf.image.decode_png(file_contents, channels=3)
-        return example, label
-
-    def getNextBatch(self):
-        # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        # load #batchSize images & labels and return as tensors
         fileNamesBatch = self._fileNames[self._index_in_epoch:self._index_in_epoch + self._batchSize]
         labelsBatch = self._labels[self._index_in_epoch:self._index_in_epoch + self._batchSize]
+        imgList = tf.convert_to_tensor(fileNamesBatch, dtype=dtypes.string)
+        labels = tf.convert_to_tensor(labelsBatch, dtype=dtypes.string)
 
-        # need to load/convert images as tensors
-        filenameQueue = tf.train.string_input_producer(fileNamesBatch)
+        # Load images
+        filenameQueue = tf.train.string_input_producer(imgList)
 
         reader = tf.WholeFileReader()
-        tmpList = []
+        images = []
         for _ in xrange(self._batchSize):
             imgKey, imgValue = reader.read(filenameQueue)
             img = tf.image.decode_jpeg(imgValue)
-            tmpList.append(img)
-        images = tf.convert_to_tensor(tmpList)
-        labels = tf.convert_to_tensor(labelsBatch, dtype=tf.string)
+            images.append(img)
 
         self._index_in_epoch += self._batchSize
 
         # Epoch completed
-        if self._index_in_epoch >= self._noImages:
+        if self._index_in_epoch >= len(self._fileNames):
             self._epochs_completed += 1
             self._index_in_epoch = 0
 
             # Shuffle data
-            perm = np.arange(self._noImages)
-            np.random.shuffle(perm)
-            self._fileNames = self._fileNames[perm]
-            self._labels = self._labels[perm]
+            tmpList = list(zip(self._fileNames, self._labels))
+            np.random.shuffle(tmpList)
+            self._fileNames, self._labels = zip(*tmpList)
 
         return images, labels
 
+    def getNextBatch(self):
+
+        fileNames = self._fileNames[self._index_in_epoch:self._index_in_epoch + self._batchSize]
+        labels = self._labels[self._index_in_epoch:self._index_in_epoch + self._batchSize]
+
+        # Load images
+        images = []
+        for file in fileNames:
+            img = misc.imread(file)
+            images.append(img)
+
+        self._index_in_epoch += self._batchSize
+
+        # Epoch completed
+        if self._index_in_epoch >= len(self._fileNames):
+            self._epochs_completed += 1
+            self._index_in_epoch = 0
+
+            # Shuffle data
+            tmpList = list(zip(self._fileNames, self._labels))
+            np.random.shuffle(tmpList)
+            self._fileNames, self._labels = zip(*tmpList)
+
+        return images, labels
 
 
 if __name__ == '__main__':
     os.chdir("..")
 
-    loader = ImageLoader(filename)
+    loader = ImageLoader(filename, 2)
 
-    # # Reads pathes of images together with their labels
-    # image_list, label_list = loader.read_labeled_image_list(filename)
+    # Reads paths of images together with their labels
+    loader.read_labeled_image_list(filename)
+
+    while loader.getEpochsCompleted() < num_epochs:
+        print("Epoch: " + str(loader._epochs_completed + 1) + "\tIndex: " + str(loader._index_in_epoch))
+        images, labels = loader.getNextBatch()
+
+        # for i in xrange(loader.getBatchSize()):
+        #     print("\tLabel: " + str(labels[i]))
+        #     print("\tShape: " + str(images[i].shape))
+
+
+    # init_op = tf.initialize_all_variables()
+    # sess = tf.Session()
+    # with sess.as_default():
+    #     while loader.getEpochsCompleted() < num_epochs:
+    #         print("Epoch: " + str(loader._epochs_completed + 1) + "\tIndex: " + str(loader._index_in_epoch))
+    #         images, labels = loader.getNextBatchTF()
     #
-    # images = tf.convert_to_tensor(image_list, dtype=tf.string)
-    # labels = tf.convert_to_tensor(label_list, dtype=tf.string)
+    #         sess.run(init_op)
+    #         coord = tf.train.Coordinator()
+    #         threads = tf.train.start_queue_runners(sess=sess, coord=coord)
     #
-    # # Makes an input queue
-    # input_queue = tf.train.slice_input_producer([images, labels],
-    #                                             num_epochs=num_epochs,
-    #                                             shuffle=True)
+    #         images1, labels1 = sess.run([images, labels])
     #
-    image, label = loader.read_images_from_disk(input_queue)
-
-
-    sess = tf.Session()
-    sess.run(tf.initialize_all_variables())
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
-
-    for i in range():  # length of your filename list
-        image = my_img.eval()  # here is your image Tensor :)
-
-    print(image.shape)
-    Image.show(Image.fromarray(np.asarray(image)))
-
-    coord.request_stop()
-    coord.join(threads)
-
-
-    # Optional Preprocessing or Data Augmentation
-    # tf.image implements most of the standard image augmentation
-    # image = preprocess_image(image)
-    # label = preprocess_label(label)
-
-    # Optional Image and Label Batching
-    # image_batch, label_batch = tf.train.batch([image, label],
-    #                                           batch_size=batch_size)
+    #         for i in xrange(loader.getBatchSize()):
+    #             print("\tLabel: " + str(labels1[i]))
+    #             print("\tShape: " + str(images1[i].shape))
+    #
+    #             # for l in labels1:
+    #             #     print(l)
+    #
+    #             # for i in range():  # length of your filename list
+    #             #     image = my_img.eval()  # here is your image Tensor :)
+    #             #
+    #             # print(image.shape)
+    #             # Image.show(Image.fromarray(np.asarray(image)))
+    #
+    #     coord.request_stop()
+    #     coord.join(threads)
