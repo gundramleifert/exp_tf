e3f3c04ce2c3f1743314020168198ca8a0afeffd
TobiasGruening <tobias.gruening.hro@gmail.com>
2016-12-07 17:28:59 +0100

    Util Classes

---
+++ util/LoaderUtil.py
@@ -0,0 +1,85 @@
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import misc
+import STR2CTC
+import os
+
+def read_image_list(pathToList):
+    """Reads a .txt file containing paths to the images
+    Args:
+       image_list_file: a .txt file with one /path/to/image per line
+       label: optionally, if set label will be pasted after each line
+    Returns:
+       List with all filenames in file image_list_file
+    """
+    f = open(pathToList, 'r')
+    filenames = []
+    for line in f:
+        filenames.append(line[:-1])
+    f.close()
+    return filenames
+
+def get_batch_labels(bList, cm):
+    labels = []
+    for path in bList:
+        labelFile = path[:] + ".txt"
+        tmp = open(labelFile, 'r')
+        str = tmp.readline();
+        labels.append(str)
+        #print(str)
+        if tmp is not None:
+            tmp.close()
+    idx, val, shape = STR2CTC.target_string_list_to_ctc_tensor_repr(labels, cm)
+    return idx, val, shape
+
+def get_batch_imgs(bList, imgW, mvn):
+    imgs = []
+    seqL = []
+
+    for path in bList:
+        aImg = misc.imread(path)
+        width = aImg.shape[1]
+        hei = aImg.shape[0]
+        aSeqL = min(width, imgW)
+        aSeqL = max(aSeqL, imgW/2)
+        seqL.append(aSeqL)
+       # aImg = aImg.astype('float32')
+        aImg = aImg / 255.0
+        if mvn:
+            std = np.std(aImg)
+            mean = np.mean(aImg)
+            tImg = (aImg - mean) / std
+            aImg = tImg
+        if width < imgW:
+            padW = imgW - width
+            npad = ((0,0), (0,padW))
+            tImg = np.pad(aImg, npad, mode='constant', constant_values=0)
+            aImg = tImg
+        if width > imgW:
+            tImg = aImg[:,:imgW]
+            aImg = tImg
+        #plt.imshow(aImg, cmap=plt.cm.gray)
+        #plt.show()
+        imgs.append(aImg)
+        bSize = len(bList)
+        imgBatched = np.zeros((bSize, hei, imgW, 1), dtype='float32')
+        # batch the image list
+        for idx, img in enumerate(imgs):
+            imgBatched[idx,:,:,0] = img
+    return imgBatched, seqL
+
+def get_list_vals(bList, cm, imgW, mvn=False):
+    tgtIdx, tgtVal, tgtShape = get_batch_labels(bList, cm)
+    inpBatch, inpSeqL = get_batch_imgs(bList, imgW, mvn)
+    return inpBatch, inpSeqL, tgtIdx, tgtVal, tgtShape
+
+if __name__ == '__main__':
+    os.chdir("..")
+    list = read_image_list('./resources/lp_only_train.lst')
+    imgBatches, seqL = get_list_vals(list, STR2CTC.get_charmap_lp(), 100)
+    #print(seqL)
+    print(imgBatches.shape)
+    print(imgBatches.dtype)
+    plt.imshow(imgBatches[129], cmap=plt.cm.gray)
+    plt.show()
---
+++ util/STR2CTC.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import tensorflow as tf
+import string
+
+
+
+
+def target_string_to_ctc_repr(targetString, charMap):
+    aTgt = []
+    for c in targetString:
+        aTgt.append(charMap[c])
+    return aTgt
+
+def target_string_list_to_ctc_tensor_repr(targetList, charMap):
+    res = []
+    for tgt in targetList:
+        encoded = tgt.decode('utf-8')
+        aTgt = []
+        for c in encoded:
+            aTgt.append(charMap[c])
+        res.append(aTgt)
+
+    #print res
+    return target_list_to_sparse_tensor_repr(res)
+
+
+def target_list_to_sparse_tensor_repr(targetList):
+    """
+    Args:
+    Inputs:
+     List of lists with the MAPPED int representations of the chars
+    Returns:
+     Tuple necessary to build the sparse tensor
+    """
+    indices=[]
+    vals = []
+    for tI, target in enumerate(targetList):
+        for seqI, val in enumerate(target):
+            indices.append([tI, seqI])
+            vals.append(val)
+    shape = [len(targetList), np.asarray(indices).max(0)[1] + 1]
+    return (np.array(indices), np.array(vals), np.array(shape))
+
+
+
+def get_charmap_lp():
+    classes = 0
+    cm = {str(x): x for x in range(10)}
+    classes += 10
+    for idx, Z in enumerate(string.ascii_uppercase):
+        cm[Z] = idx + 10
+        classes += 1
+    cm[u'Ä'] = len(cm)
+    classes += 1
+    cm[u'Ü'] = len(cm)
+    classes += 1
+    cm[u'Ö'] = len(cm)
+    classes += 1
+    finIdx = len(cm)
+    cm['-'] = finIdx
+    cm['_'] = finIdx
+    cm[' '] = finIdx
+    cm[u'.'] = finIdx
+    cm['.'] = finIdx
+    classes += 1
+    return cm, classes
+
+def get_charmap_dft():
+    cm = {}
+    for idx, Z in enumerate(string.printable):
+        cm[Z] = idx
+    return cm
