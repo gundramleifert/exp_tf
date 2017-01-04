976374ba2ab539537973bd155e25004e56211d87
Gundram Leifert <gundram.leifert@uni-rostock.de>
2017-01-04 13:40:31 +0100

    add distributed file

---
+++ models/toyexamples/toyexample_05_serverfile.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import sys
+
+
+def get_cluster_spec():
+    return tf.train.ClusterSpec({
+        "worker": [
+            # "139.30.31.218:2222",  # citlab
+            "139.30.31.13:2222", # citlab 2
+            # "139.30.31.187:8888:2222", # citlab 3
+        ],
+        "ps": [
+            "139.30.31.176:2222"  # gundram
+        ]})
+
+
+if __name__ == '__main__':
+    spec = get_cluster_spec()
+    for arg in sys.argv[1:]:
+        print arg
+    jobname = sys.argv[1]
+    taskindex = int(sys.argv[2])
+    print("job_name = '{}'".format(jobname))
+    print("task_index = '{}'".format(taskindex))
+    server = tf.train.Server(spec, job_name=jobname, task_index=taskindex)
---
+++ models/toyexamples/toyexample_06_distributed.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+from toyexample_05_distributed import ModelHandler
+from tensorflow.examples.tutorials.mnist import input_data
+import toyexample_05_serverfile as serverfile
+
+# Flags for defining the tf.train.Server
+tf.app.flags.DEFINE_string("job_name", "ps", "One of 'ps', 'worker'")
+tf.app.flags.DEFINE_integer("task_index", 0, "Index of task within the job")
+
+# Flags for defining the tf.train.ClusterSpec
+tf.app.flags.DEFINE_string("ps_hosts", "",
+                           "Comma-separated list of hostname:port pairs")
+tf.app.flags.DEFINE_string("worker_hosts", "",
+                           "Comma-separated list of hostname:port pairs")
+
+FLAGS = tf.app.flags.FLAGS
+
+
+def main(_):
+    ps_hosts = FLAGS.ps_hosts.split(",")
+    worker_hosts = FLAGS.worker_hosts.split(",")
+
+    # Create a cluster from the parameter server and worker hosts.
+    # if  len(ps_hosts)==0:
+    if len(ps_hosts) == 1 and ps_hosts[0] == '':
+        cluster = serverfile.get_cluster_spec()
+        print("use predefined cluster specification")
+    else:
+        cluster_spec = {"ps": ps_hosts, "worker": worker_hosts}
+        print(cluster_spec)
+        cluster = tf.train.ClusterSpec(cluster_spec)
+
+    # Create and start a server for the local task.
+    server = tf.train.Server(cluster,
+                             job_name=FLAGS.job_name,
+                             task_index=FLAGS.task_index)
+
+    if FLAGS.job_name == "ps":
+        server.join()
+    elif FLAGS.job_name == "worker":
+
+        # Assigns ops to the local worker by default.
+        with tf.device(tf.train.replica_device_setter(
+                worker_device="/job:worker/task:%d" % FLAGS.task_index,
+                cluster=cluster)):
+            # Data
+            mnist = input_data.read_data_sets("private/resources/MNIST_data/", one_hot=True, reshape=False,
+                                              validation_size=0)
+            # initialization
+            X = tf.placeholder(tf.float32, [None, 28, 28, 1])
+            # placeholder for correct answers
+            Y_ = tf.placeholder(tf.float32, [None, 10])
+
+            # Build model...
+            mh = ModelHandler
+            global_step = tf.Variable(0)
+            logit, Y = mh.inference(X)
+            loss = mh.loss(logit, Y, Y_, include_summary=True)
+            # minimize = mh.training(loss)
+            train_op = tf.train.AdagradOptimizer(0.01).minimize(
+                loss, global_step=global_step)
+
+            saver = tf.train.Saver()
+            summary_op = tf.summary.merge_all()
+            init_op = tf.global_variables_initializer()
+
+        # Create a "supervisor", which oversees the training process.
+        sv = tf.train.Supervisor(is_chief=(FLAGS.task_index == 0),
+                                 logdir="/tmp/train_logs",
+                                 init_op=init_op,
+                                 summary_op=summary_op,
+                                 saver=saver,
+                                 global_step=global_step,
+                                 save_model_secs=600)
+
+        # The supervisor takes care of session initialization, restoring from
+        # a checkpoint, and closing when done or an error occurs.
+        with sv.managed_session(server.target) as sess:
+            # Loop until the supervisor shuts down or 1000000 steps have completed.
+            step = 0
+            while not sv.should_stop() and step < 1000000:
+                batch_X, batch_Y = mnist.train.next_batch(100)
+                train_data = {X: batch_X, Y_: batch_Y}
+
+                # Run a training step asynchronously.
+                # See `tf.train.SyncReplicasOptimizer` for additional details on how to
+                # perform *synchronous* training.
+                _, step = sess.run([train_op, global_step], feed_dict=train_data)
+
+        # Ask for all the services to stop.
+        sv.stop()
+
+
+if __name__ == "__main__":
+    tf.app.run()
