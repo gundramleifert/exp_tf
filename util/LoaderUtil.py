56ea99b2d716009e431c59212318991dd82a5f34
Max-Weidemann <max.weidemann@hotmail.de>
2017-01-04 12:45:30 +0100

    Bugfix of #4 and #3.

--- util/LoaderUtil.py
+++ util/LoaderUtil.py
@@ -44,12 +44,15 @@ def get_batch_imgs(bList, imgW, mvn):
     imgs = []
     seqL = []
 
+    print("\n")
     for path in bList:
+        print(path)
         aImg = misc.imread(path)
         width = aImg.shape[1]
         hei = aImg.shape[0]
-        aSeqL = min(width, imgW)
-        aSeqL = max(aSeqL, imgW / 2)
+        # aSeqL = min(width, imgW)
+        # aSeqL = max(aSeqL, imgW / 2)
+        aSeqL = width
         seqL.append(aSeqL)
         # aImg = aImg.astype('float32')
         aImg = aImg / 255.0
@@ -74,6 +77,7 @@ def get_batch_imgs(bList, imgW, mvn):
         # batch the image list
         for idx, img in enumerate(imgs):
             imgBatched[idx, :, :, 0] = img
+
     return imgBatched, seqL
 
 
@@ -83,12 +87,14 @@ def get_list_vals(bList, cm, imgW, mvn=False):
     return inpBatch, inpSeqL, tgtIdx, tgtVal, tgtShape
 
 
-def clean_list(list, imgW, cm):
+def clean_list(list, imgW, cm, subsampling=-1):
     res = []
     # Count the skipped Images (width too big)
     countW = 0
     # Count the skipped Images (char not in charMap)
     countC = 0
+    # Count the skipped Images (subsampling too much)
+    countS = 0
     for path in list:
         aImg = misc.imread(path)
         width = aImg.shape[1]
@@ -103,21 +109,32 @@ def clean_list(list, imgW, cm):
         labelFile = path[:] + ".txt"
         tmp = codecs.open(labelFile, 'r', encoding='utf-8')
         u_str = tmp.readline()
+        if subsampling > 0:
+            if subsampling * len(u_str) > width:
+                countS += 1
+                continue
         if tmp is not None:
             tmp.close()
+        count = 0
+        lastCh = -1
         for c in u_str:
             try:
-                cm.get_channel(c)
+                ch = cm.get_channel(c)
+                if lastCh == ch:
+                    count += 1
+                lastCh = ch
             except KeyError:
                 # print('Character \'{}\' not in charMap, skipping Image...'.format(c))
                 skip = True
                 countC += 1
                 break
-
         if not skip:
+            if subsampling * (count + len(u_str)) > width:
+                countS += 1
+                continue
             res.append(path)
-    print("Skipped {} out of {} images...".format(countC+countW, len(list)))
-    print("...{} too big images and additionally {} images with unknown characters.".format(countW, countC))
+    print("Skipped {} out of {} images...".format(countC + countW + countS, len(list)))
+    print("...{} too big images, {} images where subsampling is too much and additionally {} images with unknown characters.".format(countW, countS, countC))
     return res
 
 
