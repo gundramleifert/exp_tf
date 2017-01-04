a3bd2e75ac27615198930737b7ab4cb4bbc9816e
Gundram Leifert <gundram.leifert@uni-rostock.de>
2017-01-04 16:49:41 +0100

    set saver to None

--- models/toyexamples/toyexample_06_distributed.py
+++ models/toyexamples/toyexample_06_distributed.py
@@ -51,8 +51,11 @@ def main(_):
 
         # Assigns ops to the local worker by default.
         with tf.device(tf.train.replica_device_setter(
-                worker_device="/job:worker/task:%d" % FLAGS.task_index,
+                worker_device="/job:ps/replica:0/task:0/cpu:0" % FLAGS.task_index,
                 cluster=cluster)):
+        # with tf.device(tf.train.replica_device_setter(
+        #         worker_device="/job:worker/task:%d" % FLAGS.task_index,
+        #         cluster=cluster)):
             # Data
 
             # Build model...
