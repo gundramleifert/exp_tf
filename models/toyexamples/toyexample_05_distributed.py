552916facf17818fc53ffd8cb89045c0c2d18165
Gundram Leifert <gundram.leifert@uni-rostock.de>
2017-01-17 15:56:15 +0100

    add working example

--- models/toyexamples/toyexample_05_distributed.py
+++ models/toyexamples/toyexample_05_distributed.py
@@ -1,140 +1,175 @@
+'''
+Distributed Tensorflow example of using data parallelism and share model parameters.
+Trains a simple sigmoid neural network on mnist for 20 epochs on three machines using one parameter server. 
+
+Change the hardcoded host urls below with your own hosts. 
+Run like this: 
+
+pc-01$ python example.py --job-name="ps" --task_index=0 
+pc-02$ python example.py --job-name="worker" --task_index=0 
+pc-03$ python example.py --job-name="worker" --task_index=1 
+pc-04$ python example.py --job-name="worker" --task_index=2 
+
+More details here: ischlag.github.io
+'''
+
+from __future__ import print_function
+
 import tensorflow as tf
+import sys
+import time
+
+# cluster specification
+parameter_servers = ["139.30.31.13:2222"]
+workers = ["139.30.31.176:2222"]
+cluster = tf.train.ClusterSpec({"ps": parameter_servers, "worker": workers})
+
+# input flags
+tf.app.flags.DEFINE_string("job_name", "worker", "Either 'ps' or 'worker'")
+tf.app.flags.DEFINE_integer("task_index", 0, "Index of task within the job")
+FLAGS = tf.app.flags.FLAGS
+
+# start a server for a specific task
+server = tf.train.Server(cluster,
+                         job_name=FLAGS.job_name,
+                         task_index=FLAGS.task_index)
+
+# config
+batch_size = 10000
+learning_rate = 0.001
+training_epochs = 20
+logs_path = "/tmp/mnist/1"
+
+# load mnist data set
 from tensorflow.examples.tutorials.mnist import input_data
-import os
-from util.saver import PrefixSaver
 
-"""
-A model is created (in scope 'net') and all weights are saved by the saver.
-Take a look at the main program - if you set load_weights=True the calculated weights are taken. To load the weights into another network have a look at toyexample_04_1_restore
-"""
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
+if FLAGS.job_name == "ps":
+    server.join()
+elif FLAGS.job_name == "worker":
 
-class ModelHandler:
-    def __init__(self):
-        pass
+    # Between-graph replication
+    with tf.device(tf.train.replica_device_setter(
+            worker_device="/job:worker/task:%d" % FLAGS.task_index,
+            cluster=cluster)):
 
-    def inference(self, X):
-        # initialization
-        # X = tf.placeholder(tf.float32, [None, 28, 28, 1])
-        # placeholder for correct answers
-        """
+        # count the number of updates
+        global_step = tf.get_variable('global_step', [],
+                                      initializer=tf.constant_initializer(0),
+                                      trainable=False)
 
-        :rtype: logit, softmax
-        """
-        K = 200
-        L = 100
-        M = 60
-        N = 30
-        # VARIABLES WITH PARAMETER SERVERS
-        number = 1
-        with tf.name_scope("net"):
-            # with tf.device("/job:ps/task:0"):
-            with tf.name_scope("l" + str(number)):
-                W1 = tf.Variable(tf.truncated_normal([784, K], stddev=0.1))
-                B1 = tf.Variable(tf.zeros([K]))
-            number += 1
-            with tf.name_scope("l" + str(number)):
-                W2 = tf.Variable(tf.truncated_normal([K, L], stddev=0.1))
-                B2 = tf.Variable(tf.zeros([L]))
-            number += 1
-            # with tf.device("/job:ps/task:1"):
-            with tf.name_scope("l" + str(number)):
-                W3 = tf.Variable(tf.truncated_normal([L, M], stddev=0.1))
-                B3 = tf.Variable(tf.zeros([M]))
-            number += 1
-            with tf.name_scope("l" + str(number)):
-                W4 = tf.Variable(tf.truncated_normal([M, N], stddev=0.1))
-                B4 = tf.Variable(tf.zeros([N]))
-            number += 1
-            with tf.name_scope("l" + str(number)):
-                W5 = tf.Variable(tf.truncated_normal([N, 10], stddev=0.1))
-                B5 = tf.Variable(tf.zeros([10]))
-        # model
-        with tf.device("/job:worker/task:0"):
-            XX = tf.reshape(X, [-1, 28 * 28])  # Input Layer
-            Y1 = tf.nn.sigmoid(tf.matmul(XX, W1) + B1)  # Hidden Layer 1
-            Y2 = tf.nn.sigmoid(tf.matmul(Y1, W2) + B2)  # Hidden Layer 2
-            Y3 = tf.nn.sigmoid(tf.matmul(Y2, W3) + B3)  # Hidden Layer 3
-            Y4 = tf.nn.sigmoid(tf.matmul(Y3, W4) + B4)  # Hidden Layer 4
-            HypLogit = tf.matmul(Y4, W5) + B5  # Output Layer
-            Hyp = tf.nn.softmax(HypLogit)
-        return HypLogit, Hyp
+        # input images
+        with tf.name_scope('input'):
+            # None -> batch size can be any size, 784 -> flattened mnist image
+            x = tf.placeholder(tf.float32, shape=[None, 784], name="x-input")
+            # target 10 output classes
+            y_ = tf.placeholder(tf.float32, shape=[None, 10], name="y-input")
 
-    def loss(self, hyp_logit, hyp, gt, include_summary=False):
-        # with tf.device("/job:worker/task:0"):
-        # loss function
-        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(hyp_logit, gt)
-        cross_entropy = tf.reduce_mean(cross_entropy)
+        # model parameters will change during training so we use tf.Variable
+        tf.set_random_seed(1)
+        with tf.name_scope("weights"):
+            W1 = tf.Variable(tf.random_normal([784, 100]))
+            W2 = tf.Variable(tf.random_normal([100, 10]))
 
-        # % of correct answers found in batch
-        correct_prediction = tf.equal(tf.argmax(gt, 1), tf.argmax(hyp, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        if include_summary:
-            tf.summary.scalar("acc", accuracy)
-            tf.summary.scalar("entropy", cross_entropy)
-        return cross_entropy, accuracy
-        # training step
-        # learning_rate = 0.003
+        # bias
+        with tf.name_scope("biases"):
+            b1 = tf.Variable(tf.zeros([100]))
+            b2 = tf.Variable(tf.zeros([10]))
 
-    def training(self, cross_entropy):
-        # with tf.device("/job:worker/task:0"):
-        optimizer = tf.train.AdamOptimizer()
-        loss = optimizer.minimize(cross_entropy)
-        return loss
+        # implement model
+        with tf.name_scope("softmax"):
+            # y is our prediction
+            z2 = tf.add(tf.matmul(x, W1), b1)
+            a2 = tf.nn.sigmoid(z2)
+            z3 = tf.add(tf.matmul(a2, W2), b2)
+            y = tf.nn.softmax(z3)
+
+        # specify cost function
+        with tf.name_scope('cross_entropy'):
+            # this is our cost
+            cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+        # specify optimizer
+        with tf.name_scope('train'):
+            # optimizer is an "operation" which we can execute in a session
+            grad_op = tf.train.GradientDescentOptimizer(learning_rate)
+
+            rep_op = tf.train.SyncReplicasOptimizer(grad_op,
+                                                    replicas_to_aggregate=10,
+                                                    replica_id=FLAGS.task_index,
+                                                    total_num_replicas=len(workers),
+                                                    use_locking=True
+                                                    )
+            train_op = rep_op.minimize(cross_entropy, global_step=global_step)
+
+            # train_op = grad_op.minimize(cross_entropy, global_step=global_step)
 
 
-if __name__ == '__main__':
-    os.chdir("../..")  # to root path
-    mnist = input_data.read_data_sets("resources/MNIST_data/", one_hot=True, reshape=False, validation_size=0)
-    epochs = 1000
-    load_weights = False
-    ############# BEGIN SAVER #############
-    prefix_saver = PrefixSaver("net", './private/models/toyexample_05')
-    ############# END SAVER #############
+        init_token_op = rep_op.get_init_tokens_op()
+        chief_queue_runner = rep_op.get_chief_queue_runner()
 
-    X = tf.placeholder(tf.float32, [None, 28, 28, 1])
-    GT = tf.placeholder(tf.float32, [None, 10])
+        with tf.name_scope('Accuracy'):
+            # accuracy
+            correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    mh = ModelHandler()
-    Ylogits, Y = mh.inference(X)
-    entropy, accuracy = mh.loss(Ylogits, Y, GT)
-    minimize = mh.training(entropy)
-    init = tf.global_variables_initializer()
-    sess = tf.Session("grpc://worker0.example.com:2222")
-    sess.run(init)
-    ############# BEGIN SAVER #############
-    if load_weights:
-        prefix_saver.restore(sess)
-    ############# END SAVER #############
-    for i in range(epochs):
-        # Load batch of images and correct answers
-        batch_X, batch_Y = mnist.train.next_batch(100)
-        train_data = {X: batch_X, GT: batch_Y}
+        # create a summary for our cost and accuracy
+        tf.scalar_summary("cost", cross_entropy)
+        tf.scalar_summary("accuracy", accuracy)
 
-        # train
-        sess.run(minimize, feed_dict=train_data)
+        # merge all summaries into a single "operation" which we can execute in a session
+        summary_op = tf.merge_all_summaries()
+        init_op = tf.initialize_all_variables()
+        print("Variables initialized ...")
 
-        # success? add code to print it
-        if (i + 1) % 100 == 0:
-            a, c = sess.run([accuracy, entropy], feed_dict=train_data)
-            print("Accuray on train set (i = " + str(i + 1) + "): " + str(a))
-            ############# BEGIN SAVER #############
-            prefix_saver.save(sess)  # , global_step=i + 1)
-            ############# END SAVER #############
-            # success on test data?
-            test_data = {X: mnist.test.images, GT: mnist.test.labels}
-            a, c = sess.run([accuracy, entropy], feed_dict=test_data)
-            print("Accuray on test set (i = " + str(i + 1) + "): " + str(a))
-    if False:
-        spec = tf.train.ClusterSpec({
-            "worker": [
-                "worker0.example.com:2222",
-                "worker1.example.com:2222",
-                "worker2.example.com:2222"
-            ],
-            "ps": [
-                "ps0.example.com:2222",
-                "ps1.example.com:2222"
-            ]})
-        for key, value in spec.as_dict().iteritems():
-            print("key = {}; value = {}".format(key, value))
+    sv = tf.train.Supervisor(is_chief=(FLAGS.task_index == 0),
+                             global_step=global_step,
+                             init_op=init_op)
+
+    begin_time = time.time()
+    frequency = 100
+    with sv.prepare_or_wait_for_session(server.target) as sess:
+
+        # is chief
+        if FLAGS.task_index == 0:
+          sv.start_queue_runners(sess, [chief_queue_runner])
+          sess.run(init_token_op)
+
+        # create log writer object (this will log on every machine)
+        writer = tf.train.SummaryWriter(logs_path, graph=tf.get_default_graph())
+
+        # perform training cycles
+        start_time = time.time()
+        for epoch in range(training_epochs):
+
+            # number of batches in one epoch
+            batch_count = int(mnist.train.num_examples / batch_size)
+
+            count = 0
+            for i in range(batch_count):
+                batch_x, batch_y = mnist.train.next_batch(batch_size)
+
+                # perform the operations we defined earlier on batch
+                _, cost, summary, step = sess.run(
+                    [train_op, cross_entropy, summary_op, global_step],
+                    feed_dict={x: batch_x, y_: batch_y})
+                writer.add_summary(summary, step)
+
+                count += 1
+                if count % frequency == 0 or i + 1 == batch_count:
+                    elapsed_time = time.time() - start_time
+                    start_time = time.time()
+                    print("Step: %d," % (step + 1),
+                          " Epoch: %2d," % (epoch + 1),
+                          " Batch: %3d of %3d," % (i + 1, batch_count),
+                          " Cost: %.4f," % cost,
+                          " AvgTime: %3.2fms" % float(elapsed_time * 1000 / frequency))
+                    count = 0
+
+        print("Test-Accuracy: %2.2f" % sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+        print("Total Time: %3.2fs" % float(time.time() - begin_time))
+        print("Final Cost: %.4f" % cost)
+
+    sv.stop()
+    print("done")
