70571b221f84592edce4e0dc6784aec071b376d3
Gundram Leifert <gundram.leifert@uni-rostock.de>
2017-01-19 17:04:09 +0100

    test distri first time

--- models/htr_iam/bdlstm_iam_v4.py
+++ models/htr_iam/bdlstm_iam_v4.py
@@ -17,41 +17,6 @@ import time
 import numpy as np
 # import matplotlib.pyplot as plt
 # import warnings
-
-# Goes down to 10%
-INPUT_PATH_TRAIN = './private/data/iam/lists/iam_train.lst'
-INPUT_PATH_VAL = './private/data/iam/lists/iam_test.lst'
-cm = get_cm_iam()
-# Additional NaC Channel
-nClasses = cm.size() + 1
-
-nEpochs = 150
-batchSize = 64
-# learningRate = 0.001
-# momentum = 0.9
-# It is assumed that the TextLines are ALL saved with a consistent height of imgH
-imgH = 32  # 64
-# Depending on the size the image is skipped or zero padded
-imgW = 2048  # 4096
-image_depth = 1
-nHiddenLSTM1 = 512
-# Needs to be consistent with subsampling [X] in the model to correctly clean up the data
-subsampling = 12
-
-os.chdir("../..")
-
-trainList = read_image_list(INPUT_PATH_TRAIN)
-valList = read_image_list(INPUT_PATH_VAL)
-print("Cleaning up train list:")
-trainList = clean_list(trainList, imgW, cm, subsampling)
-print("Cleaning up validation list:")
-valList = clean_list(valList, imgW, cm, subsampling)
-
-numT = 2048  # number of training samples per epoch
-stepsPerEpochTrain = numT / batchSize
-stepsPerEpochVal = len(valList) / batchSize
-
-
 def inference(images, seqLen, keep_prob, phase_train):
     """
 
@@ -148,100 +113,184 @@ def loss(logits3d, tgt, seqLenAfterConv):
     return loss
 
 
-print('Defining graph')
-graph = tf.Graph()
-with graph.as_default():
-    ####Graph input
-    inputX = tf.placeholder(tf.float32, shape=(batchSize, imgH, imgW, image_depth))
-    targetIxs = tf.placeholder(tf.int64)
-    targetVals = tf.placeholder(tf.int32)
-    targetShape = tf.placeholder(tf.int64)
-    targetY = tf.SparseTensor(targetIxs, targetVals, targetShape)
-    seqLengths = tf.placeholder(tf.int32, shape=(batchSize))
-    keep_prob = tf.placeholder(tf.float32)
-    trainIN = tf.placeholder_with_default(tf.constant(False), [])
-    logits3d, seqAfterConv = inference(inputX, seqLengths, keep_prob, trainIN)
-    loss = loss(logits3d, targetY, seqAfterConv)
-    saver = PrefixSaver('network', './private/models/iam_01/')
-    # optimizer = tf.train.MomentumOptimizer(learningRate, momentum).minimize(loss)
-    optimizer = tf.train.AdamOptimizer().minimize(loss)
-    # pred = tf.to_int32(ctc.ctc_beam_search_decoder(logits3d, seqAfterConv, merge_repeated=False)[0][0])
-    pred = tf.to_int32(ctc.ctc_greedy_decoder(logits3d, seqAfterConv)[0][0])
-    edist = tf.edit_distance(pred, targetY, normalize=False)
-    tgtLens = tf.to_float(tf.size(targetY.values))
-    err = tf.reduce_sum(edist) / tgtLens
+# Goes down to 10%
+INPUT_PATH_TRAIN = './private/data/iam/lists/iam_train.lst'
+INPUT_PATH_VAL = './private/data/iam/lists/iam_test.lst'
+cm = get_cm_iam()
+# Additional NaC Channel
+nClasses = cm.size() + 1
 
-with tf.Session(graph=graph) as session:
-    # writer = tf.train.SummaryWriter('./log', session.graph)
-    print('Initializing')
-    tf.global_variables_initializer().run()
-    for epoch in range(nEpochs):
-        workList = trainList[:]
-        shuffle(workList)
-        workList = workList[0:numT]
-        print('Epoch', epoch + 1, '...')
-        lossT = 0
-        errT = 0
-        timeTS = time.time()
-        tTL = 0
-        for bStep in range(stepsPerEpochTrain):
-            bList, workList = workList[:batchSize], workList[batchSize:]
-            timeTemp = time.time()
-            batchInputs, \
-            batchSeqLengths, \
-            batchTargetIdxs, \
-            batchTargetVals, \
-            batchTargetShape = get_list_vals(
-                bList,
-                cm,
-                imgW,
-                mvn=True)
-            tTL += time.time() - timeTemp
-            feedDict = {inputX: batchInputs, targetIxs: batchTargetIdxs, targetVals: batchTargetVals,
-                        targetShape: batchTargetShape, seqLengths: batchSeqLengths, keep_prob: 0.5, trainIN: True}
-            _, lossB, aErr = session.run([optimizer, loss, err], feed_dict=feedDict)
-            lossT += lossB
-            errT += aErr
-        print('Train: CTC-loss ', lossT)
-        cerT = errT / stepsPerEpochTrain
-        print('Train: CER ', cerT)
-        print('Train: time ', time.time() - timeTS)
-        print('Time for loading train data: ', tTL)
-        workList = valList[:]
-        errV = 0
-        lossV = 0
-        timeVS = time.time()
-        tVL = 0
-        for bStep in range(stepsPerEpochVal):
-            bList, workList = workList[:batchSize], workList[batchSize:]
-            timeTemp = time.time()
-            batchInputs, \
-            batchSeqLengths, \
-            batchTargetIdxs, \
-            batchTargetVals, \
-            batchTargetShape = get_list_vals(
-                bList,
-                cm,
-                imgW,
-                mvn=True
-            )
-            tVL += time.time() - timeTemp
-            feedDict = {inputX: batchInputs, targetIxs: batchTargetIdxs, targetVals: batchTargetVals,
-                        targetShape: batchTargetShape, seqLengths: batchSeqLengths, keep_prob: 1.0, trainIN: False}
-            lossB, aErr = session.run([loss, err], feed_dict=feedDict)
-            # lossB, aErr, sE, sL = session.run([loss, err, err_val, loss_val], feed_dict=feedDict)
-            # writer.add_summary(sE, epoch*stepsPerEpocheVal + bStep)
-            # writer.add_summary(sL, epoch * stepsPerEpocheVal + bStep)
-            lossV += lossB
-            errV += aErr
-        print('Val: CTC-loss ', lossV)
-        errVal = errV / stepsPerEpochVal
-        print('Val: CER ', errVal)
-        print('Val: time ', time.time() - timeVS)
-        print('Time for loading validation data: ', tVL)
-        # Write a checkpoint every 10 epochs
-        if (epoch+1) % 10 == 0:
-            saveTime =  time.time()
-            print('Saving...')
-            saver.save(session, global_step=epoch)
-            print('Time for saving: ', time.time() - saveTime)
+nEpochs = 150
+batchSize = 64
+# learningRate = 0.001
+# momentum = 0.9
+# It is assumed that the TextLines are ALL saved with a consistent height of imgH
+imgH = 32  # 64
+# Depending on the size the image is skipped or zero padded
+imgW = 2048  # 4096
+image_depth = 1
+nHiddenLSTM1 = 512
+# Needs to be consistent with subsampling [X] in the model to correctly clean up the data
+subsampling = 12
+
+#PARAMETER FOR DISTRIBUTED
+sync_replica = True
+num_agregate = 100
+
+parameter_servers = ["139.30.31.176:2222"]
+workers = ["139.30.31.13:2223",
+           "139.30.31.176:2224", "139.30.31.176:2223"]
+cluster = tf.train.ClusterSpec({"ps": parameter_servers, "worker": workers})
+
+# input flags
+tf.app.flags.DEFINE_string("job_name", "", "Either 'ps' or 'worker'")
+tf.app.flags.DEFINE_integer("task_index", "", "Index of task within the job")
+FLAGS = tf.app.flags.FLAGS
+
+# start a server for a specific task
+server = tf.train.Server(cluster,
+                         job_name=FLAGS.job_name,
+                         task_index=FLAGS.task_index)
+
+if FLAGS.job_name == "ps":
+    server.join()
+elif FLAGS.job_name == "worker":
+
+    os.chdir("../..")
+
+    trainList = read_image_list(INPUT_PATH_TRAIN)
+    valList = read_image_list(INPUT_PATH_VAL)
+    print("Cleaning up train list:")
+    trainList = clean_list(trainList, imgW, cm, subsampling)
+    print("Cleaning up validation list:")
+    valList = clean_list(valList, imgW, cm, subsampling)
+
+    numT = 2048  # number of training samples per epoch
+    stepsPerEpochTrain = numT / batchSize
+    stepsPerEpochVal = len(valList) / batchSize
+
+
+    # Between-graph replication
+    with tf.device(tf.train.replica_device_setter(
+            worker_device="/job:worker/task:%d" % FLAGS.task_index,
+            cluster=cluster)):
+        # count the number of updates
+        global_step = tf.get_variable('global_step', [],
+                                      initializer=tf.constant_initializer(0),
+                                      trainable=False)
+
+        print('Defining graph')
+        graph = tf.Graph()
+        with graph.as_default():
+            ####Graph input
+            inputX = tf.placeholder(tf.float32, shape=(batchSize, imgH, imgW, image_depth))
+            targetIxs = tf.placeholder(tf.int64)
+            targetVals = tf.placeholder(tf.int32)
+            targetShape = tf.placeholder(tf.int64)
+            targetY = tf.SparseTensor(targetIxs, targetVals, targetShape)
+            seqLengths = tf.placeholder(tf.int32, shape=(batchSize))
+            keep_prob = tf.placeholder(tf.float32)
+            trainIN = tf.placeholder_with_default(tf.constant(False), [])
+            logits3d, seqAfterConv = inference(inputX, seqLengths, keep_prob, trainIN)
+            loss = loss(logits3d, targetY, seqAfterConv)
+            saver = PrefixSaver('network', './private/models/iam_05_distributed/')
+            # optimizer = tf.train.MomentumOptimizer(learningRate, momentum).minimize(loss)
+            grad_op = tf.train.AdamOptimizer()
+
+            if sync_replica:
+                if num_agregate < 0:
+                    num_agregate = len(workers)
+                rep_op = tf.train.SyncReplicasOptimizerV2(grad_op,
+                                                          replicas_to_aggregate=num_agregate,
+                                                          # replica_id=FLAGS.task_index,
+                                                          total_num_replicas=len(workers),
+                                                          use_locking=True
+                                                          )
+                train_op = rep_op.minimize(loss, global_step=global_step)
+            else:
+                train_op = grad_op.minimize(loss, global_step=global_step)
+
+        if sync_replica:
+            init_token_op = rep_op.get_init_tokens_op()
+            chief_queue_runner = rep_op.get_chief_queue_runner()
+
+            # pred = tf.to_int32(ctc.ctc_beam_search_decoder(logits3d, seqAfterConv, merge_repeated=False)[0][0])
+            pred = tf.to_int32(ctc.ctc_greedy_decoder(logits3d, seqAfterConv)[0][0])
+            edist = tf.edit_distance(pred, targetY, normalize=False)
+            tgtLens = tf.to_float(tf.size(targetY.values))
+            err = tf.reduce_sum(edist) / tgtLens
+
+        with tf.Session(graph=graph) as session:
+            # writer = tf.train.SummaryWriter('./log', session.graph)
+            print('Initializing')
+            tf.global_variables_initializer().run()
+            for epoch in range(nEpochs):
+                workList = trainList[:]
+                shuffle(workList)
+                workList = workList[0:numT]
+                print('Epoch', epoch + 1, '...')
+                lossT = 0
+                errT = 0
+                timeTS = time.time()
+                tTL = 0
+                for bStep in range(stepsPerEpochTrain):
+                    bList, workList = workList[:batchSize], workList[batchSize:]
+                    timeTemp = time.time()
+                    batchInputs, \
+                    batchSeqLengths, \
+                    batchTargetIdxs, \
+                    batchTargetVals, \
+                    batchTargetShape = get_list_vals(
+                        bList,
+                        cm,
+                        imgW,
+                        mvn=True)
+                    tTL += time.time() - timeTemp
+                    feedDict = {inputX: batchInputs, targetIxs: batchTargetIdxs, targetVals: batchTargetVals,
+                                targetShape: batchTargetShape, seqLengths: batchSeqLengths, keep_prob: 0.5, trainIN: True}
+                    _, lossB, aErr = session.run([grad_op, loss, err], feed_dict=feedDict)
+                    lossT += lossB
+                    errT += aErr
+                print('Train: CTC-loss ', lossT)
+                cerT = errT / stepsPerEpochTrain
+                print('Train: CER ', cerT)
+                print('Train: time ', time.time() - timeTS)
+                print('Time for loading train data: ', tTL)
+                workList = valList[:]
+                errV = 0
+                lossV = 0
+                timeVS = time.time()
+                tVL = 0
+                for bStep in range(stepsPerEpochVal):
+                    bList, workList = workList[:batchSize], workList[batchSize:]
+                    timeTemp = time.time()
+                    batchInputs, \
+                    batchSeqLengths, \
+                    batchTargetIdxs, \
+                    batchTargetVals, \
+                    batchTargetShape = get_list_vals(
+                        bList,
+                        cm,
+                        imgW,
+                        mvn=True
+                    )
+                    tVL += time.time() - timeTemp
+                    feedDict = {inputX: batchInputs, targetIxs: batchTargetIdxs, targetVals: batchTargetVals,
+                                targetShape: batchTargetShape, seqLengths: batchSeqLengths, keep_prob: 1.0, trainIN: False}
+                    lossB, aErr = session.run([loss, err], feed_dict=feedDict)
+                    # lossB, aErr, sE, sL = session.run([loss, err, err_val, loss_val], feed_dict=feedDict)
+                    # writer.add_summary(sE, epoch*stepsPerEpocheVal + bStep)
+                    # writer.add_summary(sL, epoch * stepsPerEpocheVal + bStep)
+                    lossV += lossB
+                    errV += aErr
+                print('Val: CTC-loss ', lossV)
+                errVal = errV / stepsPerEpochVal
+                print('Val: CER ', errVal)
+                print('Val: time ', time.time() - timeVS)
+                print('Time for loading validation data: ', tVL)
+                # Write a checkpoint every 10 epochs
+                if (epoch+1) % 10 == 0:
+                    saveTime =  time.time()
+                    print('Saving...')
+                    saver.save(session, global_step=epoch)
+                    print('Time for saving: ', time.time() - saveTime)
