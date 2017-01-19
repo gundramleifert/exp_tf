1292926b507b3aa08b6fdfb37322078e308930bc
Gundram Leifert <gundram.leifert@uni-rostock.de>
2017-01-19 16:38:10 +0100

    change replica optimizer

--- models/toyexamples/toyexample_05_distributed.py
+++ models/toyexamples/toyexample_05_distributed.py
@@ -101,7 +101,7 @@ elif FLAGS.job_name == "worker":
                     num_agregate = len(workers)
                 rep_op = tf.train.SyncReplicasOptimizerV2(grad_op,
                                                         replicas_to_aggregate=num_agregate,
-                                                        replica_id=FLAGS.task_index,
+                                                        # replica_id=FLAGS.task_index,
                                                         total_num_replicas=len(workers),
                                                         use_locking=True
                                                         )
